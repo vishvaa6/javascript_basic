f1b08e3c617bca2f7ed4f17ef2abfee14533dc5f
vishvaa6 <vishvaav006@gmail.com>
2024-09-01 15:22:12 +0530

    Add files via upload

---
+++ random_number_generator/index.js
@@ -0,0 +1,20 @@
+const button= document.getElementById("button1");
+const label1= document.getElementById("label1");
+const label2= document.getElementById("label2");
+const label3= document.getElementById("label3");
+const max = 6;
+const min = 1;
+let randomnum1;
+let randomnum2;
+let randomnum3;
+
+
+button.onclick = function(){
+randomnum1 = Math.floor(Math.random()*max)+min;
+randomnum2 = Math.floor(Math.random()*max)+min;
+randomnum3 = Math.floor(Math.random()*max)+min;
+label1.textContent =randomnum1;
+label2.textContent = randomnum2;
+label3.textContent = randomnum3;
+}
+
